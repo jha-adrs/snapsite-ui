6975c7fa1274b918f42d193784715ef25a534e08
Aadarsh <aadarshjha6783@gmail.com>
2024-02-13 19:43:08 +0530

    Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Toaster />
+            <SpeedInsights/>
             <TooltipProvider>
               {children}
               <Analytics/>
